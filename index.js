d99c82bcee50cb9d0c28da837aa8ba8c85a3ed07
Antony11659 <shurupov.tosha93@gmail.com>
2024-12-12 01:56:47 +0300

    to do list for crating users and creating participators tables

--- index.js
+++ index.js
@@ -36,7 +36,7 @@ bot.onText(/\/start(.*)/, async (msg, match) => {
     } else if (isUserSupporter) {
       const friend = await selectUser(friendId);
       const friendName = friend[0].username;
-      const userHasSubscribedBefore = (await selectUser(userId).length) > 0; // check if a user who clicked the link had been subscribed before.
+      const userHasSubscribedBefore = (await selectUser(userId).length) > 0; // check if a user who clicked the link has been subscribed before.
       if (userHasSubscribedBefore) {
         bot.sendMessage(
           chatId,
@@ -47,6 +47,8 @@ bot.onText(/\/start(.*)/, async (msg, match) => {
           chatId,
           `You supported your friend ${friendName} successfully`
         );
+        // you should push a friend into participators
+        // you should push a supporter into users
         bot.sendMessage(friendId, `You was supported by ${userName}`);
       }
     }
@@ -68,86 +70,6 @@ bot.onText(/\/newLot/, async (msg) => {
       ],
     },
   };
+  // create a new participators table
   await bot.sendMessage(channelId, Lot.text, options);
 });
-
-// bot.on("message", async (msg) => {
-//   const chatId = msg.chat.id;
-//   const name = msg.chat.first_name;
-//   const userId = msg.from.id;
-//   const userStatus = (await bot.getChatMember(channelId, msg.from.id)).status;
-//   const referralLink = `https://t.me/anykka_test_bot?start=${userId}`;
-//   const argument = match[1].trim();
-//   const participatorId = Number(argument);
-//   const userResponse = msg.text;
-//   if (userResponse === "support") {
-//     bot.sendMessage(userId, "You supported your friend");
-//   }
-// });
-
-// bot.onText(/\/start(.*)/, async (msg, match) => {
-//   const chatId = msg.chat.id;
-//   const name = msg.chat.first_name;
-//   const userId = msg.from.id;
-//   const userStatus = (await bot.getChatMember(channelId, msg.from.id)).status;
-//   const referralLink = `https://t.me/anykka_test_bot?start=${userId}`;
-//   const argument = match[1].trim();
-//   const participatorId = Number(argument);
-
-//   if (argument === "participator") {
-//     bot.sendMessage(
-//       chatId,
-//       `Hello ${name} if you want to participate in this giveaway ask one friend to join our channel https://t.me/test_anykka_electronics and your friend should click to this link ${referralLink} !'`
-//     );
-//   } else if (participatorId) {
-//     if (
-//       // don't forget to delete admin and creator
-//       userStatus === "administrator" ||
-//       userStatus === "creator" ||
-//       userStatus === "member"
-//     ) {
-//       // check if a supporter is new. or he has already been in my group
-//       const user = await selectUser(userId); // check if a user who clicked the link had been subscribed before.
-//       if (user.length > 0) {
-//         bot.sendMessage(
-//           userId,
-//           "Sorry you cant support your fried because you are or you were subscribed"
-//         );
-//         bot.sendMessage(
-//           participatorId,
-//           `Sorry your friend ${name} can't support you he has supported someone else`
-//         );
-//       } else {
-//         await insertUser(name, userId);// add the participator to the users list
-//         bot.sendMessage(userId, "You supported your fried successfully");
-//         bot.sendMessage(
-//           participatorId,
-//           `Your friend ${name} supported you successfully!`
-//         );
-//       }
-//     } else {
-//       bot.sendMessage(
-//         userId,
-//         "Bro you haven't subscribed please subscribe https://t.me/test_anykka_electronics and come again! "
-//       );
-//     }
-//   } else {
-//     bot.sendMessage(chatId, "Hello do you want to participate? ");
-//   }
-// });
-
-// bot.onText(/\participate/, async (msg) => {
-//   const options = {
-//     reply_markup: {
-//       inline_keyboard: [
-//         [
-//           {
-//             text: "button",
-//             url: "https://t.me/anykka_test_bot?start=participator",
-//           },
-//         ],
-//       ],
-//     },
-//   };
-//   await bot.sendMessage(channelId, "Hello", options);
-// });
